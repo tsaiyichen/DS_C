9820a778f249780612d54f174c104b94ac722d17
tsaiyichen <easonjessiejj@gmail.com>
2024-07-30 12:38:37 +0800

    stack

--- stack.c
+++ stack.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdbool.h>
 
-struct StackNode{
+typedef struct StackNode{
     int data;
     struct StackNode* next;
-};
+}stackNode_t;
 
-struct StackNode* newNode(int x){
-    struct StackNode* nodePointer = (struct StackNode*)malloc(sizeof(struct stackNode));
+stackNode_t* newNode(int x){
+    stackNode_t* nodePointer = (stackNode_t*)malloc(sizeof(stackNode_t));
     nodePointer->data = x;
     nodePointer->next = NULL;
     return nodePointer;
 }
 
-bool isEmpty(struct StackNode* root){
+bool isEmpty(stackNode_t* root){
     return !root;
 }
-void push(struct StackNode** root, int x){
-    struct StackNode* t = newNode(x);
+void push(stackNode_t** root, int x){
+    stackNode_t* t = newNode(x);
     t->next = *root;
     *root = t;
-    printf("push %d into stack.", x);
+    printf("push %d into stack.\n", x);
 }
 
-int pop(structNode** root){
+int pop(stackNode_t** root){
     if(isEmpty(*root)){
-        printf("Stack Empty.");
+        printf("Stack Empty.\n");
     }else{
-        struct StackNode* t = *root;
+        stackNode_t* t = *root;
         int x = t->data;
         *root = t->next;
         free(t);
-        return item;
+        return x;
     }
 }
 
 int main(void){
-    struct StackNode* stack = NULL;
-    struct StackNode** root = &stack;
+    stackNode_t* stack = NULL;
+    stackNode_t** root = &stack;
 
     push(root, 1);
+    printf("%d\n", pop(root));
     push(root, 2);
     push(root, 3);
+    printf("%d\n", pop(root));
+    printf("%d\n", pop(root));
     push(root, 4);
 }
