0af73523535d7902836f2d8a58c81168d6c32973
tsaiyichen <easonjessiejj@gmail.com>
2024-08-16 04:16:16 +0800

    tree node traversal algo. 0815

--- Ch5/binaryTree.cpp
+++ Ch5/binaryTree.cpp
@@ -24,11 +24,22 @@ private:
     TreeNode* root;
     void inorder_traversal(treenode_t *root);
     void postorder_traversal(treenode_t *root);
+    void preorder_traversal(treenode_t *root);
+    TreeNode* copyTree(treenode_t *root);
+    bool isEqual(treenode_t *s, treenode_t *t);
+    int countTotalNodes(treenode_t *root);
+    int countLeafNodes(treenode_t *root);
 public:
     BinaryTree();
     BinaryTree(string arr[], int size);
+    BinaryTree(BinaryTree &bt);
+    TreeNode* getRoot();
     void inorder_traversal();
     void postorder_traversal();
+    void preorder_traversal();
+    bool isEqual(BinaryTree &bt);
+    int countTotalNodes();
+    int countLeafNodes();
 };
 
 BinaryTree::BinaryTree() : root(nullptr) {}
@@ -65,6 +76,25 @@ BinaryTree::BinaryTree(string arr[], int size) {
     }
 }
 
+TreeNode *BinaryTree::getRoot() {
+    return this->root;
+}
+
+TreeNode* BinaryTree::copyTree(treenode_t *root) {
+    if(root == NULL){
+        return nullptr;
+    }else{
+        treenode_t* temp = new TreeNode(root->data);
+        temp->lchild = copyTree(root->lchild);
+        temp->rchild = copyTree(root->rchild);
+        return temp;
+    }
+}
+
+BinaryTree::BinaryTree(BinaryTree &bt) {
+    this->root = copyTree(bt.getRoot());
+}
+
 void BinaryTree::inorder_traversal(treenode_t* root) {
     if(root == NULL){
         return;
@@ -84,6 +114,59 @@ void BinaryTree::postorder_traversal(treenode_t *root) {
        cout << root->data;
    }
 }
+
+void BinaryTree::preorder_traversal(treenode_t *root) {
+    if(root == NULL){
+        return;
+    }else{
+        cout << root->data;
+        preorder_traversal(root->lchild);
+        preorder_traversal(root->rchild);
+    }
+}
+
+bool BinaryTree::isEqual(treenode_t *s, treenode_t *t) {
+    bool res = false;
+    if(s == NULL && t == NULL){
+        res = true;
+    }else if(s != NULL && t != NULL){
+        if(s->data == t->data){
+            if(isEqual(s->lchild, t->lchild)){
+                if(isEqual(s->rchild, t->rchild)){
+                    res = true;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int BinaryTree::countTotalNodes(treenode_t *root) {
+    if(root == NULL) return 0;
+    return countTotalNodes(root->lchild) + countTotalNodes(root->rchild) + 1;
+}
+int BinaryTree::countLeafNodes(treenode_t* root) {
+    if(root == NULL) return 0;
+    if(root->lchild == NULL && root->rchild == NULL){
+        return 1;
+    }else{
+        return countLeafNodes(root->lchild) + countLeafNodes(root->rchild);
+    }
+}
+
+int BinaryTree::countLeafNodes() {
+    return countLeafNodes(this->root);
+}
+int BinaryTree::countTotalNodes() {
+    return countTotalNodes(this->root);
+}
+bool BinaryTree::isEqual(BinaryTree &bt) {
+    return isEqual(this->root, bt.getRoot());
+}
+void BinaryTree::preorder_traversal() {
+    preorder_traversal(this->root);
+    cout << endl;
+}
 void BinaryTree::inorder_traversal() {
     inorder_traversal(this->root);
     cout << endl;
@@ -95,8 +178,21 @@ void BinaryTree::postorder_traversal() {
 
 
 int main(){
-    string array[7] = {"A", "B", "C", "", "E", "", "G"};
-    BinaryTree bt1(array, 7);
+    string bt1Arr[10] = {"A", "B", "C", "D", "E", "", "G", "", "", "H"};
+    int arrayLength = sizeof(bt1Arr) / sizeof(bt1Arr[0]);
+    BinaryTree bt1(bt1Arr, arrayLength);
+    cout << "BT1: " << endl;
     bt1.inorder_traversal();
     bt1.postorder_traversal();
+    bt1.preorder_traversal();
+    BinaryTree bt2(bt1);
+    cout << "BT2: " << endl;
+    bt2.inorder_traversal();
+    bt2.postorder_traversal();
+    bt2.preorder_traversal();
+    cout << "BT1 vs BT2: " <<  bt1.isEqual(bt2) << " (same: 1, different: 0)" << endl;
+    cout << "the number of total nodes of BT1: " << bt1.countTotalNodes() << endl;
+    cout << "the number of total \"leaf\" nodes of BT1: " << bt1.countLeafNodes() << endl;
+
+
 }
